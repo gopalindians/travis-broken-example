f922f39709f089935aa371874027ae45805fd045
Gopal Sharma <gopalindians@gmail.com>
2016-12-26 20:23:17 +0530

    Added New Test

--- Test.php
+++ Test.php
@@ -2,9 +2,15 @@
 
 class Test extends PHPUnit_Framework_TestCase
 {
-	public function testOnePlusOne() {
-		$this->assertEquals(1,1);
-  	}
+    public function testOnePlusOne()
+    {
+        $this->assertEquals(1, 1);
+    }
+
+    public function testThreePlusThree()
+    {
+        $this->assertEquals(3 + 3, 6);
+    }
 }
 
 ?>
